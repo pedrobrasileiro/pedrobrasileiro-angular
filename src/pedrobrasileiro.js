712ecda3100ff61d057b576ab22633b67b16eb11
Pedro Brasileiro <pedrobrasileiro@gmail.com>
2016-07-20 13:11:13 -0300

    Fix directives

--- src/pedrobrasileiro.js
+++ src/pedrobrasileiro.js
@@ -8,30 +8,30 @@ angular.module('pedrobrasileiro', [
 
 
 // Directives
-angular.module('pedrobrasileiro.directives').directive('focusMe', function($timeout) {
+angular.module('pedrobrasileiro.directives').directive('focusMe', ['$timeout', function(t) {
   return {
     link: function(scope, element, attrs) {
-      $timeout(function() {
+      t(function() {
         element[0].focus();
       }, 150);
     }
   };
-});
+}]);
 
 // Utils
-angular.module('pedrobrasileiro.utils').factory('$localStorage', ['$window', function($window) {
+angular.module('pedrobrasileiro.utils').factory('$localStorage', ['$window', function(win) {
   return {
     set: function(key, value) {
-      $window.localStorage[key] = value;
+      win.localStorage[key] = value;
     },
     get: function(key, defaultValue) {
-      return $window.localStorage[key] || defaultValue;
+      return win.localStorage[key] || defaultValue;
     },
     setObject: function(key, value) {
-      $window.localStorage[key] = JSON.stringify(value);
+      win.localStorage[key] = JSON.stringify(value);
     },
     getObject: function(key) {
-      return $window.localStorage[key]?JSON.parse($window.localStorage[key]):null;
+      return win.localStorage[key]?JSON.parse(win.localStorage[key]):null;
     }
   };
 }]);
